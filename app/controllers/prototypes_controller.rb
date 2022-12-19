0ba4a90f146ded54ba9d898eca198fd9d81fc4bd
noplan31 <paparyu31@icloud.com>
2022-12-19 21:32:17 +0900

    エラーの解消1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,6 +3,8 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
 
   before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -54,5 +56,9 @@ class PrototypesController < ApplicationController
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
